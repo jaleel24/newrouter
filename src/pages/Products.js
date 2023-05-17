a2c2f8a4678f7c4a35cfcabbc3cb8f676623c80b
jaleel sayal <jaleelsayal@hotmail.com>
2023-05-16 21:54:54 -0400

    added a dummy products and dynamically created the list of products using map

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,22 @@
-
+import { Link } from "react-router-dom";
 function Products() {
+
+    const PRODUCTS =[
+        {id:1,Title:'Product 1'},
+        {id:2,Title:'Product 2'},
+        {id:3,Title:'Product 3'},
+    ];
     return <>
         <h1>This is the Products page</h1>
         <ul>
-            <li>Chettos</li>
-            <li>Lays</li>
-            <li>Doritos</li>
+            {PRODUCTS.map(products =>{
+                return (<li key={products.id}>
+                    <Link to={`/products/${products.id}`}>{products.Title}</Link>
+                </li>)
+            })}
+            {/* <Link to="/products"><li>Product 1</li></Link>
+            <Link><li>Product 2</li></Link>
+            <Link><li>Product 3</li></Link> */}
         </ul>
     </>
     
